28256f0e200712d25cec48b8a9b2bd0ddb726f5d
Vitor Bansen - Mailou.Cloud <vitor.bansen@mailou.com.br>
2026-09-13 23:59:51 -0300

    first commit1

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-inter",
-});
+}); 
 
 export const metadata: Metadata = {
   title: `${siteConfig.nome} | ${siteConfig.tagline}`,
